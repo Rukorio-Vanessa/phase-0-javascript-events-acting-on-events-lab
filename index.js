2aa3ff9f7d92a9606e8b9813da0c347b0d041da0
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-11-22 22:23:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+
+// moves the dodger to the left;
+function moveDodgerLeft() {
+    var dodger = document.getElementById("dodger")
+    var left = parseInt(dodger.style.left)
+    left -= 30
+    dodger.style.left = left+"px";
+
+}
+//moves the dodger to the right
+function moveDodgerRight() {
+    var dodger = document.getElementById("dodger")
+    var left = parseInt(dodger.style.left)
+    left += 40
+    dodger.style.left = left+"px";
+}    
